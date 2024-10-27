d0b166a219f0fd1882469574d2c530546f72b6a6
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-28 01:21:56 +0500

    change

--- app_book/views.py
+++ app_book/views.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from app_book.models import BookModel
@@ -8,3 +9,11 @@ class BookListView(APIView):
     def get(self, request):
         books = BookModel.objects.all()
         serializer = BookModelSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BookModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
