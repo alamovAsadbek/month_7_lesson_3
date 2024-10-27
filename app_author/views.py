0b8fa14e6bf404eb35632d6c1887de6bfc5a088c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-28 01:54:31 +0500

    change

--- app_author/views.py
+++ app_author/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializer import *
+
+
+class AuthorView(APIView):
+    def get(self, request):
+        authors = AuthorModel.objects.all()
+        serializer = AuthorModelSerializer(authors, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AuthorModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
