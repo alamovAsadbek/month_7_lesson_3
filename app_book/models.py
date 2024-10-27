f955af6a04fd319e233b57dd10e587ba3dc1c40c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-28 01:05:48 +0500

    change

--- app_book/models.py
+++ app_book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class BookModel(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    author = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+    def __str__(self):
+        return self.title
