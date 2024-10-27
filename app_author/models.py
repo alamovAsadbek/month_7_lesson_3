5bd06ecdaf17134e76c11ebffa409986a37adf1c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-28 01:04:16 +0500

    change model

--- app_author/models.py
+++ app_author/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from common.models import BaseModel
 
-class AuthorModel(models.Model):
+
+class AuthorModel(BaseModel):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
